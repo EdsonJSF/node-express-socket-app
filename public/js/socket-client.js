25633aea2a247d7ef69671cc67c15b196dce1597
EdsonJSF <edsonjsf1990@gmail.com>
2023-09-01 16:27:47 -0400

    Feat: socket.on connect and disconnect

--- public/js/socket-client.js
+++ public/js/socket-client.js
@@ -1,1 +1,18 @@
-const socketClient = io()
+const lblOnline = document.querySelector("#lblOnline");
+const lblOffline = document.querySelector("#lblOffline");
+
+const socketClient = io();
+
+socketClient.on("connect", () => {
+  console.log("front connect");
+
+  lblOnline.classList.remove("d-none");
+  lblOffline.classList.add("d-none");
+});
+
+socketClient.on("disconnect", () => {
+  console.log("front disconnect");
+
+  lblOnline.classList.add("d-none");
+  lblOffline.classList.remove("d-none");
+});
